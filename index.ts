2068f42a8afa993210b97bd544d11dbcf704c84d
Ilshat Sultanov <ilshat@sultanov.team>
2022-07-06 03:22:53 +0500

    Add wrappers

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export {evalString, evalFile} from './lib/neoguard.js'
+import * as neoguard from './lib/neoguard.js';
+
+export const evalString = (text: string) => neoguard.evalString(text);
+export const evalFile = (url: string) => neoguard.evalFile(url);
