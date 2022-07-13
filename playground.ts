5151a12f9bceac17312c92f111eb951529bc1969
Ilshat Sultanov <ilshat@sultanov.team>
2022-07-13 14:05:37 +0500

    Update playground

--- playground.ts
+++ playground.ts
@@ -1,27 +1,52 @@
-import {evalString, generateCaptcha, generateCaptchaWithMax} from "./index.ts";
+import {
+  asMarkdownCodeBlock,
+  asMarkdownCodeBlockWithPrefix,
+  evalString,
+  generateCaptcha,
+  generateCaptchaWithLimit,
+} from "./index.ts";
 
 console.log("evalString: correct samples");
-["(+ 1 2)", "(inc 41)"].map((s) => console.log(s, "=>", evalString(s)))
+["(+ 1 2)", "(inc 41)", '(str "foo-" 42)'].map((s) =>
+  console.log(s, "=>", evalString(s))
+);
 
-console.log("\n")
+console.log("\n");
+console.log("evalString: correct samples [as markdown]");
+["(+ 1 2)", "(inc 41)", '(str "foo-" 42)'].map((s) =>
+  console.log(s, "=>", asMarkdownCodeBlock(evalString(s)))
+);
+
+console.log("\n");
 console.log("evalString: malformed samples");
 ["", "(+ 1", "(foo 41)"].map((s) => {
   try {
     const res = evalString(s);
-    console.log(s, "=>", res)
+    console.log(s, "=>", res);
   } catch (err) {
-    console.log(s, "=>", err.message)
+    console.log(s, "=>", err.message);
   }
-})
+});
 
-console.log("\n")
-console.log("generateCaptcha:")
+console.log("\n");
+console.log("evalString: malformed samples [as markdown]");
+["(+ 1", "(foo 41)"].map((s) => {
+  try {
+    const res = evalString(s);
+    console.log(s, "=>", res);
+  } catch (_) {
+    console.log(s, "=>", asMarkdownCodeBlockWithPrefix("Malformed input:", s));
+  }
+});
+
+console.log("\n");
+console.log("generateCaptcha:");
 for (let n = 0; n < 5; n++) {
-  console.log("=>", generateCaptcha())
+  console.log("=>", generateCaptcha());
 }
 
-console.log("\n")
-console.log("generateCaptchaWithMax:")
+console.log("\n");
+console.log("generateCaptchaWithLimit:");
 for (let n = 0; n < 25; n = n + 5) {
-  console.log("max", n, "=>", generateCaptchaWithMax(n))
+  console.log("max", n, "=>", generateCaptchaWithLimit(n));
 }
